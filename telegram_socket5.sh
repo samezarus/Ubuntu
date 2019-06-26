3e0599dd20fc18456835907d6b8c47c262f63433
sameza <samezarus@gmail.com>
2019-06-26 13:30:15 +1000

    Update telegram_socket5.sh
    5

--- telegram_socket5.sh
+++ telegram_socket5.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
 # zabbix inf
-# UTM
-token='000000000000000000000000000000'
-chat='1111111'
 
-subj='subject'
-message='message'
+clear
+
+adrr="$1" # token=chat exmp: 534834893:AAHrIlaxNEWti1E9vXgtANwigIjsuvpzgzc=-277889075
+subj="$2"
+message="$3"
+
+token=''
+chat=''
+
+fl=0
+for((i=0; $i<${#adrr}; i++)) 
+do 
+	if [ ${adrr:$i:1} = "=" ] 
+	then
+		fl+=1
+	fi
+	
+	if [[ "$fl" -eq 0 ]]
+	then
+		token+=${adrr:$i:1}
+	else
+		chat+=${adrr:$i+1:1}
+	fi
+done
+
+# UTM
+#token='534834893:AAHrIlaxNEWti1E9vXgtANwigIjsuvpzgzc'
+#chat='-277889075'
+
+#subj='subject'
+#message='message'
 
 # socks5 proxy settings
 socks5_user='asd'
 socks5_pass='123'
-socks5_host='127.0,0.1'
+socks5_host='127.0.0.1'
 socks5_port='1080'
 socks5_params=${socks5_user}:${socks5_pass}@${socks5_host}:${socks5_port}
 

e93344733c53ce58277d85a872a522ffe6b11651
sameza <samezarus@gmail.com>
2019-06-26 15:19:01 +1000

    Update telegram_socket5.sh
    7

--- telegram_socket5.sh
+++ telegram_socket5.sh
@@ -34,6 +34,6 @@ socks5_user='asd'
 socks5_pass='123'
 socks5_host='127.0.0.1'
 socks5_port='1080'
-socks5_params=${socks5_user}:${socks5_pass}@${socks5_host}:${socks5_port}
+socks5_params=$socks5_user:$socks5_pass@$socks5_host:$socks5_port
 
-curl --socks5 ${socks5_params} -s -X POST https://api.telegram.org/bot${token}/sendMessage -F chat_id=${chat} -F text=${subj}$'\n'${message}
+curl --socks5 $socks5_params -s -X POST https://api.telegram.org/bot$token/sendMessage -F chat_id=$chat -F text="$subj"$'\n'"$message"

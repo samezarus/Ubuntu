790c0f027410eea8c8f5a720a30d5cf774fb2594
sameza <samezarus@gmail.com>
2019-06-26 10:03:03 +1000

    Update telegram_socket5.sh
    2

--- telegram_socket5.sh
+++ telegram_socket5.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+# zabbix inf
 token='534834893:AAHrIlaxNEWti1E9vXgtANwigIjsuvpzgzc'
 chat='-277889075'
 subj='subj'
 message='message3'
 text=${subj}\n${message}
 
+# socks5 proxy settings
+socks5_user='crypt_user'
+socks5_pass='vOhYtkOv'
+socks5_host='165.22.30.75'
+socks5_port='1080'
+
 #echo -e ${text}
-curl --socks5 crypt_user:vOhYtkOv@165.22.30.75:1080 -s -X POST https://api.telegram.org/bot${token}/sendMessage -F chat_id=${chat} -F text=${text}
+curl --socks5 ${socks5_user}:${socks5_pass}@${socks5_host}:${socks5_port}-s -X POST https://api.telegram.org/bot${token}/sendMessage -F chat_id=${chat} -F text=${text}

9df7e8ea7e45db4a0ec75571cbc72190e1ffcf0a
sameza <samezarus@gmail.com>
2019-07-12 11:44:10 +1000

    Update OpenVpn_conf2.sh
    с

--- OpenVpn_conf2.sh
+++ OpenVpn_conf2.sh
@@ -1,5 +1,3 @@
-# НЕДОДЕЛАН !!!
-
 # По мотивам статьи
 # https://www.digitalocean.com/community/tutorials/openvpn-ubuntu-16-04-ru#%D1%88%D0%B0%D0%B3-5-%D1%81%D0%BE%D0%B7%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5-%D1%81%D0%B5%D1%80%D1%82%D0%B8%D1%84%D0%B8%D0%BA%D0%B0%D1%82%D0%B0,-%D0%BA%D0%BB%D1%8E%D1%87%D0%B0-%D0%B8-%D1%84%D0%B0%D0%B9%D0%BB%D0%BE%D0%B2-%D1%88%D0%B8%D1%84%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F-%D0%B4%D0%BB%D1%8F-%D1%81%D0%B5%D1%80%D0%B2%D0%B5%D1%80%D0%B0 
 #
